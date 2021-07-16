1ce61a486086e2ed249cdc50a1f2def7dfea1b40
MaruthiMN <87351472+MaruthiMN@users.noreply.github.com>
2021-07-16 01:51:59 -0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,24 @@
+ <?php 
+
+$server="localhost";
+
+$username="root";
+
+$password=""; 
+
+$db="sbb";
+
+
+$conn=mysqli_connect($server,$username,$password,$db);
+
+
+if($conn){
+  
+}
+
+else 
+die("connection to this database failed due to " .mysqli_connect_error());
+
+
+
+?>
